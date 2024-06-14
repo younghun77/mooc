bbcfc4953927af3c28d303db8473c4f2f6fb5fdb
younghun77 <167798148+younghun77@users.noreply.github.com>
2024-06-14 22:58:43 +0900

    Update 6798.cpp

--- 6798.cpp
+++ 6798.cpp
@@ -1,47 +1,63 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int N;//정수
-vector<int> vec;
 
 void InputData(){ 
 	cin >> N; 
 }
 
+struct Fraction {
+    int molecule;
+    int denominator;
+};
+
+
+bool compareFractions(const Fraction &a, const Fraction &b) {
+    return (a.molecule * b.denominator) > (b.molecule * a.denominator);
+}
+
 int main() {
 	InputData();//입력
-	//나눴을때 1보다 작거나 같은 경우
-	for(int i=1; i<=N; i++)
+	vector<Fraction> fractions;
+	Fraction f;
+	//여기서부터 작성
+	//0값 : 0/1 및 1값 : 1/1은 제외하고 sort한 다음 front와 back에 각각 push
+	//여기서 중요한것은 중간에 있는 값들을 어떻게 정렬할 것인지 여부
+
+	//0/1, 1/1
+	//0/2, 1/2, 2/2
+	//0/3, 1/3, 2/3, 3/3
+	//0/4, 1/4, 2/4, 3/4, 4/4
+	//0/5, 1/5, 2/5, 3/5, 4/5, 5/5
+	for(int i=1; i<=N; i++) //분모(molecule)
 	{
-		for(int j=0; j/i<=1; j++)
+		for(int j=0; j<=N; j++) //분자(denominator)
 		{
 			
-			if (j/i == 0 || j/i == 1)
+			if ((j == 0 && i == 1) || (j == 1 && i == 1)) 
 			{
-				if (i!=0 || i!=1) continue;
+			    f.molecule = i;
+			    f.denominator = j;
+			    //0/1, 1/1
 			}
-			//vec push
-			vec.push_back(j/i);
-			
-			
-			//0/1, 1/1
-			//0/2, 1/2, 2/2
-			//0/3, 1/3, 2/3, 3/3
-			//0/4, 1/4, 2/4, 3/4, 4/4
-			//0/5, 1/5, 2/5, 3/5, 4/5, 5/5
-
+			else if (j !=0 || (i >= j) )
+			{
+			    //push(j/i)
+			    f.molecule = i;
+			    f.denominator = j;
+			}
+			fractions.push_back(f);
 		}
-		//나눴을때 1보다 작거나 같은 경우
-
 	}
-	//여기서부터 작성
-	//vec 정렬 및 print
-	sort(vec.begin(), vec.end());
-	for (int i=0; i<vec.size(); i++) {
-		cout <<	vec[i] << "\n";
-	}
+	sort(fractions.begin(), fractions.end(), compareFractions);
+    
+    for (const auto &fraction : fractions) 
+    {
+        cout << fraction.molecule << "/" << fraction.denominator << endl;
+    }
 	return 0;
 }

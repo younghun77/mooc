7c1a9a1acb7c4c7d1d29046d646efa9f332e3d1b
younghun77 <167798148+younghun77@users.noreply.github.com>
2024-10-27 06:25:46 +0900

    Update 도서관.cpp

--- 도서관.cpp
+++ 도서관.cpp
@@ -28,10 +28,12 @@ void solution(){
     for(int i = 0; i<N; i++){
         current_s = student[i].first;
         current_e = student[i].second;
+        //만약 이전 학생이 떠나는 시간보다 다음 학생의 도착 시간이 빠르다면 →  구간이 이어짐 : 구간의 끝값을 다시 계산해 주어야 하는데, 시작시간이 느려도 이전 학생이 더 늦게 떠날 수도 있으므로 max값으로 갱신
         if(current_s<=e){
             e = max(current_e,e);
             present = max(e-s, present);
         }
+        //하지만 다음 학생이 이전 학생이 떠난 후에 도착 →  학생이 한 명도 머물지 않았던 시간을 갱신 : 이전의 시작 시간 바꿔줌
         else{
             absent = max(current_s-e, absent);
             s = current_s;
